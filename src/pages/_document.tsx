9f4136ff97ebad8fe6a13d72e6fd18a81297e5c7
Bruno Ferreira Santos <brunoferreirasmv@gmail.com>
2021-11-16 20:31:05 -0400

    finlizado boa parte da responsividade

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
                     <link rel="shotcut icon" href="/iconBearFavicon.png" type="image/png" />
                 </Head>
                 <body>
